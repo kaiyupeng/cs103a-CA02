42fad85273af5c060f867b49375f6c548612767b
kaiyupeng <kaiyupeng@brandeis.edu>
2023-05-06 14:13:51 -0400

    Update startup.sh

--- firstapp/startup.sh
+++ firstapp/startup.sh
@@ -1,4 +1,3 @@
 # export MONGODB_URI="mongodb+srv://cs103aSpr23:oDyglzBvRw47EJAU@cluster0.kgugl.mongodb.net/?retryWrites=true&w=majority"
-export MONGODB_URI="mongodb://127.0.0.1:27017/ca02"
-export OPENAI_API_KEY="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"
+export MONGODB_URI="mongodb://127.0.0.1:27017/ca02-kaiyupeng"
 npm start
