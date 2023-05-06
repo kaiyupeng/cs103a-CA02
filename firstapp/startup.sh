c179b6c9bdc1eea63399fd271dec03913406dd34
kaiyupeng <kaiyupeng@brandeis.edu>
2023-05-06 14:14:40 -0400

    Update startup.sh

--- firstapp/startup.sh
+++ firstapp/startup.sh
@@ -1,3 +1,2 @@
-# export MONGODB_URI="mongodb+srv://cs103aSpr23:oDyglzBvRw47EJAU@cluster0.kgugl.mongodb.net/?retryWrites=true&w=majority"
 export MONGODB_URI="mongodb://127.0.0.1:27017/ca02-kaiyupeng"
 npm start
